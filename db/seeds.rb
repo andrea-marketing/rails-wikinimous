301138965cd148224ff245be6fbdfeffb4c17fbf
andrea-marketing <andrea.marketing.kedge@gmail.com>
2022-05-31 16:33:06 +0200

    wikimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  article = Article.create(
+    title: Faker::TvShows::MichaelScott.quote.to_s,
+    content: Faker::TvShows::NewGirl.quote.to_s
+  )
+  article.save
+end
